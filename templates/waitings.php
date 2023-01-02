4b96755edf44490af2b9f0bb9948a1daa71a45dc
firebird76 <12985740+firebird76@users.noreply.github.com>
2023-01-02 13:40:11 +0100

    Update waitings.php

--- templates/waitings.php
+++ templates/waitings.php
@@ -19,7 +19,7 @@ script ('ocdownloader', 'badger.min');
 script ('ocdownloader', 'ocdownloader.min');
 script ('ocdownloader', 'waitings');
 ?>
-<div id="app">
+<div id="content">
     <div id="app-navigation">
         <?php print_unescaped ($this->inc ('part.navigation')); ?>
     </div>
